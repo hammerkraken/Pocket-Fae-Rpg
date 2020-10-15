2ad913464f3e617a9b74107eb54e3d6411b5ca97
Joseph Timmons <josephwtimmons@gmail.com>
2020-10-14 20:31:45 -0400

    Working on Dialog

---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI speakerName, dialogue, navButtonText;
+    public Image speakerSprite;
+
+    private int currentIndex;
+    private Conversation currentConvo;
+    private static DialogueManager instance;
+    private Animator anim;
+
+    private void Awake()
+    {
+        if (instance = null)
+        {
+            instance = this;
+            anim = GetComponent<Animator>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public static void StartConversation(Conversation convo)
+    {
+        instance.anim.SetBool("isOpen", true);
+        instance.currentIndex = 0;
+        instance.currentConvo = convo;
+        instance.speakerName.text = "";
+        instance.dialogue.text = "";
+        instance.navButtonText.text = ">";
+        instance.ReadNext();
+
+    }
+
+    public void ReadNext()
+    {
+
+        if(currentIndex>currentConvo.GetLength())
+        {
+            instance.anim.SetBool("isOpen", false);
+            return;
+        }
+
+        speakerName.text= currentConvo.GetLineByIndex(currentIndex).speaker.GetName();
+
+        instance.StartCoroutine(TypeText(currentConvo.GetLineByIndex(currentIndex).dialogue));
+     
+        speakerSprite.sprite = currentConvo.GetLineByIndex(currentIndex).speaker.GetSprite();
+
+        currentIndex++;
+        if(currentIndex>+currentConvo.GetLength())
+        {
+            navButtonText.text = "X";
+        }
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        //using one letter at a time
+        dialogue.text = "";
+        bool complete = false;
+        int index = 0;
+
+
+        while (!complete)
+        {
+            dialogue.text += text[index];
+            index++;
+            yield return new WaitForSeconds(.02f);
+            if (index == text.Length - 1)
+                complete = true;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    public Conversation convo;
+    public void StartConvo()
+    {
+        DialogueManager.StartConversation(convo);
+    }
+}
